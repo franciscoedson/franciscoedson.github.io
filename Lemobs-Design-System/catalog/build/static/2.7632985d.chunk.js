69cf94fa1268c6b462e93180ac95092376806381
Francisco Edson <fedrosansonicc@gmail.com>
2020-05-05 18:44:54 -0300

    to heroku

--- Lemobs-Design-System/catalog/build/static/2.7632985d.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{782:function(e,t,n){var o=n(15),r=n(45),a=n(293).PageRenderer;a.__esModule&&(a=a.default);var c=r({displayName:"WrappedPageRenderer",getInitialState:function(){return{content:n(785)}},componentWillMount:function(){},render:function(){return o.createElement(a,Object.assign({},this.props,{content:this.state.content}))}});c.__catalog_loader__=!0,e.exports=c},785:function(e,t){e.exports="# Cor\n"}});
-//# sourceMappingURL=2.7632985d.chunk.js.map
